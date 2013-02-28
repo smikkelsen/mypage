c51841c244b501d4e4ba44ce1831c3aaa6f2161d
Sean Mikkelsen <smikkelsen@myebiz.com>
2013-02-28 16:43:45 -0700

    added edit buttons on page

--- app/helpers/window_items_helper.rb
+++ app/helpers/window_items_helper.rb
@@ -3,28 +3,28 @@ module WindowItemsHelper
   def aa_flyout(item, parent_layout)
     text = ''
     if item.link.empty?
-      text += "<div class='#{parent_layout} window-item-parent aa_flyout button clickable'>#{item.name}"
+      text += "<div class='#{parent_layout} window-item-parent aa_flyout button clickable'>#{item.name}\r\n"
 
       sub_items = WindowItem.by_parent(item.id).active
       unless sub_items.nil?
-        text += '  <div class="dropdown_menu_list_wrapper" style="display: none; ">'
-        text += '    <ul class="dropdown_menu_list">'
+        text += "  <div class='dropdown_menu_list_wrapper' style='display: none; '>\r\n"
+        text += link_to (image_tag 'settings_lt.png', :class => 'settings flt-rt', :width => '15px'), admin_window_item_path(item.id), :target => '_blank'
+        text += "    <ul class='dropdown_menu_list'>\r\n"
         sub_items.each do |sub|
-          text += "      <li #{hover_text_if(sub.hover_text)} class='#{item.children_layout} #{hover_class_if(sub.hover_text)} '>"
+          text += "      <li #{hover_text_if(sub.hover_text)} class='#{item.children_layout} #{hover_class_if(sub.hover_text)} '>\r\n"
           text += aa_flyout(sub, item.children_layout)
-          text += '      </li>'
+          text += "      </li>\r\n"
         end
-        text += '    </ul>'
-        text += '  </div>'
+        text += "    </ul>\r\n"
+        text += "  </div>\r\n"
       end
-      text += '</div>'
+      text += "</div>\r\n"
 
     else
       #text += link_to "<div class='#{parent_layout} window-item-parent aa_flyout button clickable'>#{item.name}", item.link
-      text += "<a class='category_link' href='#{item.link}'><div class='#{parent_layout} window-item-parent aa_flyout button clickable'>#{item.name}</div></a>"
+      text += "<a class='category_link' href='#{item.link}'><div class='#{parent_layout} window-item-parent aa_flyout button clickable'>#{item.name}</div></a>\r\n"
     end
 
   end
-
 
 end
--- app/helpers/windows_helper.rb
+++ app/helpers/windows_helper.rb
@@ -2,64 +2,73 @@ module WindowsHelper
 
   def get_search_bar_window
     text = ''
-    text += '<div style="margin:6px 0px" >'
-    text += '<form id="google-search">'
+    text += "<div style='margin:6px 0px' >\r\n"
+    text += "<form id='google-search'>\r\n"
     text += "<input type='text' class='input' id='search_string' name='search_string' tabindex='1' >"
-    text += "<button id='gbqfba' class='submit_button' aria-label='Google Search' name='btnK' style='margin:0' >Google Search</button>"
-    text += '</form>'
-    text += '</div>'
+    text += "<button id='gbqfba' class='submit_button' aria-label='Google Search' name='btnK' style='margin:0' >Google Search</button>\r\n"
+    text += "</form>\r\n"
+    text += "</div>\r\n"
   end
 
   def get_default_window(window)
     text = ''
 
-    text += '<div class="panel">'
-    text += '  <div class="panel-insert">'
-    text += '    <table class="panel-insert-table">'
-    text += '      <tr class="panel-heading">'
-    text += "        <td colspan=#{window.column_count}>#{render_icon(window.icon_id, 25, styles = 'float:left; padding: 5px;')}"
-    text += "          <h2>#{window.name}</h2></td>"
-    text += '      </tr>'
+    text += "<div class='panel'>\r\n"
+
+    text += "  <div class='panel-insert'>\r\n"
+
+    text += "    <table class='panel-insert-table'>\r\n"
+    text += "      <tr class='panel-heading'>\r\n"
+    text += "        <td colspan=#{window.column_count}>#{render_icon(window.icon_id, 25, styles = 'float:left; padding: 5px;')}\r\n"
+    text += "          <h2>#{window.name}"
+    text += link_to (image_tag 'settings_dk.png', :class => 'flt-rt', :width => '20px'), admin_window_path(window.id), :target => '_blank'
+
+    text += "</h2></td>\r\n"
+
+    text += "      </tr>\r\n"
+
+    text += "      <tr class='panel-body'>\r\n"
+
+    text += "        <td valign='top'>\r\n"
+
+    text += "          <table>\r\n"
 
     window_items = WindowItem.by_window(window.id).is_parent.active
     window_items.in_groups_of(window.column_count) do |row|
-      text += '      <tr class="panel-body">'
-
+      text += "            <tr class='searchable'>\r\n"
       row.each do |item|
-        text += '        <td valign="top">'
+        text += "              <td>\r\n"
 
         unless item.nil?
 
-          text += '          <table>'
-          text += '            <tr class="searchable">'
 
-          text += '              <td>'
-          text += "                #{render_icon(item.icon_id, 18, styles = 'padding-right:5px;')}<span #{hover_text_if(item.hover_text)} class='#{hover_class_if(item.hover_text)}'>#{link_to_unless item.link.empty?, item.name, item.link rescue '' }</span>"
-          text += '              </td>'
+          text += "                #{render_icon(item.icon_id, 18, styles = 'padding-right:5px;')}<span #{hover_text_if(item.hover_text)} class='#{hover_class_if(item.hover_text)}'>#{link_to_unless item.link.empty?, item.name, item.link rescue '' }</span>\r\n"
+          text += "              </td>\r\n"
           sub_items = WindowItem.by_parent(item.id).active
           unless sub_items.nil?
-            text += '              <td>'
+            text += "              <td>\r\n"
             sub_items.each do |sub|
               text += aa_flyout(sub, item.children_layout)
             end
-            text += '              </td>'
+            text += "              </td>\r\n"
           end
 
-          text += '            </tr>'
-          text += '          </table>'
         end
-        text += '        </td>'
 
       end # End Column
-      text += '      </tr>'
+      text += "            </tr>\r\n"
 
 
     end # End Row
+    text += "          </table>\r\n"
 
+    text += "        </td>\r\n"
 
-    text += '    </table>'
-    text += '  </div>'
-    text += '</div>'
+    text += "      </tr>\r\n"
+
+    text += "    </table>\r\n"
+    text += "  </div>\r\n"
+    text += "</div>\r\n"
 
 
     return text
